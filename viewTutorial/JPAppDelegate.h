d8f6ffb8afda62103744bdd27d7e7fb6542527f8
Josh <jpetrack@hmc.edu>
2014-09-10 16:30:40 -0700

    Initial Commit

---
+++ viewTutorial/JPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JPAppDelegate.h
+//  viewTutorial
+//
+//  Created by Josh Petrack on 9/10/14.
+//  Copyright (c) 2014 Josh Petrack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ viewTutorial/JPViewController.h
@@ -0,0 +1,13 @@
+//
+//  JPViewController.h
+//  viewTutorial
+//
+//  Created by Josh Petrack on 9/10/14.
+//  Copyright (c) 2014 Josh Petrack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPViewController : UIViewController
+
+@end
