0ad7786c49e557893ed99089959720348519fd19
Josh <jpetrack@hmc.edu>
2014-09-10 18:11:48 -0700

    Did entire project (added button, with blue text, that highlights on press).

---
+++ viewTutorial/JPGrid.h
@@ -0,0 +1,13 @@
+//
+//  JPGrid.h
+//  viewTutorial
+//
+//  Created by Josh Petrack on 9/10/14.
+//  Copyright (c) 2014 Josh Petrack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPGrid : UIView
+
+@end
---
+++ viewTutorial/JPGrid.m
@@ -0,0 +1,31 @@
+//
+//  JPGrid.m
+//  viewTutorial
+//
+//  Created by Josh Petrack on 9/10/14.
+//  Copyright (c) 2014 Josh Petrack. All rights reserved.
+//
+
+#import "JPGrid.h"
+
+@implementation JPGrid
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
